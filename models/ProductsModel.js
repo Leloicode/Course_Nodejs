7cd1b8b27628df35a7759df1bd18a50d30099d26
Leloicode <leloi2002nvt@gmail.com>
2023-04-13 11:39:56 +0700

    Up to Rest Api Product and User

---
+++ models/ProductsModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please Enter Product Name']
+        },
+        quantity: {
+            type: Number,
+            required: false,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        image: {
+            type: String,
+            required: false
+        }
+        // ,
+        // user_id: {
+        //     type: mongoose.Types.ObjectId,
+        //     required: true
+        // },
+    },
+    {
+        timestamps: true,
+    }
+
+)
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,13 +1,148 @@
-const http = require('http');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const Product = require('./models/ProductsModel')
+const User = require('./models/UserModel');
 const port = 3000;
-const server = http.createServer((req, res) => {
-    console.log('run request');
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Hello would!</h1>');
-    res.write('<h3>xin chào</h3>');
-    res.end();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// app.get('/', (req, res) => {
+//     res.send('Xin chao leloi')
+//     console.log('xin chafo');
+// });
+// app.get('/tintuc', (req, res) => {
+//     res.send('Xin chao tintuc')
+// });
 
+//product
+app.get('/Api/Product', async (req, res) => {
+    try {
+        const product = await Product.find({});
+        res.status(200).json(product);
+        console.log(product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
 })
-server.listen(port, 'localhost', () => {
-    console.log('NodeJs server listening on port: 3000');
-});
+app.get('/Api/Product/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+        console.log(product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+app.post('/Api/Product', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+app.put('/Api/Product/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const productOld = await Product.findByIdAndUpdate(id, req.body);
+        if (!product) {
+            res.status(404).json({ message: 'cannot find id product' })
+        }
+        const productNew = await Product.findById(id);
+        res.status(200).json(productNew);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+app.delete('/Api/Product/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({ message: `cannot find id product` })
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+
+
+//user
+app.get('/Api/User', async (req, res) => {
+    try {
+        const user = await User.find({});
+        res.status(200).json(user);
+        console.log(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+app.get('/Api/User/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findById(id);
+        res.status(200).json(user);
+        console.log(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+app.post('/Api/User', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+app.put('/Api/User/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const userCurrent = await User.findByIdAndUpdate(id, req.body);
+        if (!userCurrent) {
+            res.status(404).json({ message: 'cannot find id product' })
+        }
+        const userNew = await User.findById(id);
+        res.status(200).json(userNew);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+app.delete('/Api/User/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findByIdAndDelete(id);
+        if (!user) {
+            return res.status(404).json({ message: `cannot find id user` })
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+
+mongoose.set('strictQuery', false)
+mongoose.connect('mongodb+srv://testApi:loi12tren9deptrai@atlascluster.ptmjntq.mongodb.net/Product?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Example app listening on port http://localhost:${port}`)
+        });
+        console.log('Connected susscefully');
+    })
+    .catch((error) => {
+        console.log(error);
+    })
